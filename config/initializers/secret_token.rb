535936a186f9bb54368c027aeb07cfdd9a3fa85b
Kleber Infante <corujito@gmail.com>
2015-08-07 20:50:13 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mercadinho::Application.config.secret_key_base = '589e6e67982f8a1bd6fe727dece76aa6bc6eef8b3569f3be37d85b3ca5e9779edabd358ea4b757ba9b2c0138e098ebcd436e5cbe7b7e90d1eabd87827d73021a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mercadinho::Application.config.session_store :cookie_store, key: '_mercadinho_session'
