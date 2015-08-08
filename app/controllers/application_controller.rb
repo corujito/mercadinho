31f23c63cd10ab3d19174de6aa0b27c4cd6ae1b8
Kleber Infante <corujito@gmail.com>
2015-08-08 11:05:57 -0300

    layouts e troca de senha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 class ApplicationController < ActionController::Base
-  layout :layout_by_resource
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u|
+      u.permit(:email, :password, :password_confirmation, :current_password)
+    }
+  end
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  protected
-
-  def layout_by_resource
-    if devise_controller?
-      "login_layout"
-    else
-      "application"
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Mercadinho::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
+  end
   resources :clients
 
   root 'clients#index'
