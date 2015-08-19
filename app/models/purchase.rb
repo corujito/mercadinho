a3440d782aebe0638a97bc9029b24d55c81b4b7a
Kleber Infante <corujito@gmail.com>
2015-08-18 21:46:41 -0300

    validacao

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,5 +2,5 @@ class Purchase < ActiveRecord::Base
   belongs_to :vendor
   has_many :items
   accepts_nested_attributes_for :items, :reject_if => :all_blank, :allow_destroy => true
-  validates :vendor, presence: true
+  validates :discount, :items, presence: true
 end
