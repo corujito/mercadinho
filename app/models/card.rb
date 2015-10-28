78505d7c471f37b5c44d2607a4bd932b92e1be0b
Kleber Infante <corujito@gmail.com>
2015-10-28 08:18:57 -0200

    apagando registros ao apagar cartao

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ActiveRecord::Base
   validates :password, :full_name, :identification, presence: true
   paginates_per 20
 
-  has_many :in_out_cards
+  has_many :in_out_cards, :dependent => :destroy
 
-  enum card_type: [ :outros, :visa, :master, :alelo ]
+  enum card_type: [ :outros, :visa, :master, :alelo, :sodexo ]
 end
