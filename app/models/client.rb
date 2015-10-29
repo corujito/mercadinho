7a070121bde98d0f8a67c3b265483886167d6eae
Kleber Infante <corujito@gmail.com>
2015-10-29 07:44:03 -0200

    tela com detalhes de cliente

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,4 +8,8 @@ class Client < ActiveRecord::Base
   has_many :orders
   has_many :payments
   has_many :order_items, through: :orders
+
+  def estimate_profit
+    self.orders.inject(0) { |sum, o| sum + o.estimate_profit_per_product }
+  end
 end
