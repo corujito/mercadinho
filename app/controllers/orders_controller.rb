a295450266ef22171edbe9c657c589700b00b122
Kleber Infante <corujito@gmail.com>
2015-11-07 17:11:58 -0200

    performance no show de products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -70,7 +70,7 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.includes({order_items: [:product]}).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
