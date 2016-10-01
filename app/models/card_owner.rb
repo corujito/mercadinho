0ce2b5f2c31f803773f9d0416e02a77d031e213d
Kleber Infante <corujito@gmail.com>
2016-10-01 09:25:58 -0300

    correcao de validacao

--- app/models/card_owner.rb
+++ app/models/card_owner.rb
@@ -1,6 +1,6 @@
 class CardOwner < ActiveRecord::Base
   validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, uniqueness: { case_sensitive: false, scope: :in_out_type, message: "deve ser unico por tipo" }
 
   enum in_out_type: [ :in, :out ]
 
